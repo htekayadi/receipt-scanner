ddf5df10bb261d102518109198df2ca3d3668609
Hendrik Tekayadi <htekayadi@gmail.com>
2020-02-25 18:48:03 +0700

    Display yello boxes over text

--- app/components/Preview.js
+++ app/components/Preview.js
@@ -20,16 +20,46 @@ export default class Preview extends React.Component {
     })
   }
 
+  componentDidMount() {
+    const canvas = this.refs.canvas;
+    const ctx = canvas.getContext("2d");
+    
+    const img = this.refs.image;
+   
+    if(img != null) {
+      img.onload = () => {
+        ctx.drawImage(img, 0, 0, 400, 600);
+        ctx.beginPath();
+        ctx.fillStyle = "#f0ff048c";
+
+        this.props.numbers.forEach(number => {
+          ctx.fillRect(
+            number.vertices[0].x,
+            number.vertices[0].y,
+            number.vertices[1].x - number.vertices[0].x,
+            number.vertices[2].y - number.vertices[0].y);
+        })
+        ctx.stroke();
+      };
+    }
+  }
+
   render() {
     const imageUrl = this.props.imageUrls[0] ? this.props.imageUrls[0].url : ''
     return (
       <div>
         <Grid container divided streched centered>
-        <div className="previewComponent">
-                <div className="imgPreview">
-                    <img src={imageUrl} alt="" onClick={this.handleClick}/>
-                </div>
-            </div>
+          <div className="previewComponent">
+              <div className="imgPreview">
+                  <canvas ref="canvas" width={400} height={600} onClick={this.handleClick}/>
+                  <img
+                    style={{display: 'none'}}
+                    ref="image"
+                    src={imageUrl}
+                    onClick={this.handleClick}
+                  />
+              </div>
+          </div>
         </Grid>
       </div>
     );
